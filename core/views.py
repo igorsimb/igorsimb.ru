0c32716adf501f4f3bf92bd0c0c198154670ff2a
igorsimb <igor.simbirtsev@gmail.com>
2023-01-11 16:06:34 +0300

    fix(igorsimb): fix links on name and portrait
    Clicking on name and portrait in the nav menu now scrolls up to the #hero section instead of 404

--- core/views.py
+++ core/views.py
@@ -9,6 +9,14 @@ from core.forms import ContactForm
 
 User = get_user_model()
 
+# TODO:
+# Foreign key = many-to-one
+# 1. Embed EZ2Task OR find a free Heroku-like service (fly.io maybe?)
+    # 1.1 Check links to authentication system. It needs to be igorsimb's allauth
+    # 1.2 In urls.py - check all the links. Need to switch them to accounts/blabla urls (see igorsimb)
+    # 1.3 Add all additional fields to User Model from ez2task to igorsimb
+# 2. Embed Tortiki
+# 3. Embed Blog
 
 class IndexView(TemplateView):
     template_name = 'core/index.html'
