9475b5d7372d11df7f30dc6192680ab24128e3c1
igorsimb <igor.simbirtsev@gmail.com>
2023-03-01 16:50:06 +0300

    fix(settings,frontend): changed db settings, removed nav menu from ez2task page
    Currently the way a single-project page is set up makes it impossible to click on nav menu, which is both confusing and bad UX. Removed the nav menu from the ez2task page and left only the breadcrumbs' Home link

--- igorsimb/settings.py
+++ igorsimb/settings.py
@@ -96,16 +96,25 @@ WSGI_APPLICATION = 'igorsimb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'u1853808_default',
-        'USER': 'u1853808_default',
-        'PASSWORD': 'NhJxp160xyJ5DYhR',
-        'HOST': 'localhost',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'u1853808_default',
+            'USER': 'u1853808_default',
+            'PASSWORD': 'NhJxp160xyJ5DYhR',
+            'HOST': 'localhost',
+        }
+    }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
