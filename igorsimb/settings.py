0fbe0b90bf1829ec4e7e5c2319702fa62b2cf6d4
igorsimb <igor.simbirtsev@gmail.com>
2023-03-22 20:20:31 +0300

    fix(images): add upload_to param to Models' image fields

--- igorsimb/settings.py
+++ igorsimb/settings.py
@@ -164,7 +164,7 @@ STATIC_ROOT = 'static/'
 # STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_DIR = [BASE_DIR / 'static']
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
 # Default primary key field type
