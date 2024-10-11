694ae0305efed35b0f23610633c16f31f349e653
igorsimb <igor.simbirtsev@gmail.com>
2024-10-11 12:52:56 +0300

    fix(skills_list.html): add a dot in Docker section
    For consistency, since all other sections end with a dot.

--- igorsimb/urls.py
+++ igorsimb/urls.py
@@ -5,21 +5,17 @@ from django.urls import path, include
 
 urlpatterns = [
     # Django admin
-    path('admin/', admin.site.urls),
-
+    path("admin/", admin.site.urls),
     # User management
-    path('accounts/', include('allauth.urls')),
-
+    path("accounts/", include("allauth.urls")),
     # Local apps
-    path('', include('core.urls')),
-    path('store/', include('store.urls')),
-    path('', include('store_users.urls')),
-
+    path("", include("core.urls")),
+    path("store/", include("store.urls")),
+    path("", include("store_users.urls")),
     # Third-party apps
-    path('__debug__/', include('debug_toolbar.urls')),
-] + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+    path("__debug__/", include("debug_toolbar.urls")),
+    path(r"", include("puput.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-admin.site.site_header = 'Админ-панель'
-admin.site.index_title = 'Администрирование сайта'
+admin.site.site_header = "Админ-панель"
+admin.site.index_title = "Администрирование сайта"
