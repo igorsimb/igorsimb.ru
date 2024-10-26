dce359b9b8e9031a025ad794555eda646aa4ac44
igorsimb <igor.simbirtsev@gmail.com>
2024-10-26 13:10:25 +0300

    improvement(bootstrap): upgrade Bootstap from 5.2.2 to 5.3.3
    - Add blog link to main nav menu
    - Fix js script to also highlight active blog link
    - Fix tags having white bg with white text and thus being invisible.

--- static/core/js/main.js
+++ static/core/js/main.js
@@ -4,7 +4,7 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
-(function() {
+(function () {
   "use strict";
 
   /**
@@ -44,17 +44,31 @@
    * Navbar links active state on scroll
    */
   let navbarlinks = select('#navbar .scrollto', true)
+
+  const setActiveLink = (navbarlink, isActive) => {
+    if (isActive) {
+      navbarlink.classList.add('active');
+    } else {
+      navbarlink.classList.remove('active');
+    }
+  };
+
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
+
+    // Check if the current URL starts with '/blog/'
+    if (window.location.pathname.startsWith('/blog/')) {
+      navbarlinks.forEach(navbarlink => {
+        setActiveLink(navbarlink, navbarlink.getAttribute('href') === '/blog/');
+      });
+      return;
+    }
+
     navbarlinks.forEach(navbarlink => {
       if (!navbarlink.hash) return
       let section = select(navbarlink.hash)
       if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
+      setActiveLink(navbarlink, position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight));
     })
   }
   window.addEventListener('load', navbarlinksActive)
@@ -90,7 +104,7 @@
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
+  on('click', '.mobile-nav-toggle', function (e) {
     select('body').classList.toggle('mobile-nav-active')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
@@ -99,7 +113,7 @@
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  on('click', '.scrollto', function(e) {
+  on('click', '.scrollto', function (e) {
     if (select(this.hash)) {
       e.preventDefault()
 
@@ -149,7 +163,7 @@
     new Waypoint({
       element: skilsContent,
       offset: '80%',
-      handler: function(direction) {
+      handler: function (direction) {
         let progress = select('.progress .progress-bar', true);
         progress.forEach((el) => {
           el.style.width = el.getAttribute('aria-valuenow') + '%'
@@ -170,9 +184,9 @@
 
       let portfolioFilters = select('#portfolio-flters li', true);
 
-      on('click', '#portfolio-flters li', function(e) {
+      on('click', '#portfolio-flters li', function (e) {
         e.preventDefault();
-        portfolioFilters.forEach(function(el) {
+        portfolioFilters.forEach(function (el) {
           el.classList.remove('filter-active');
         });
         this.classList.add('filter-active');
@@ -180,7 +194,7 @@
         portfolioIsotope.arrange({
           filter: this.getAttribute('data-filter')
         });
-        portfolioIsotope.on('arrangeComplete', function() {
+        portfolioIsotope.on('arrangeComplete', function () {
           AOS.refresh()
         });
       }, true);
