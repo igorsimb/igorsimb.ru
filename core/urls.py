276043eaf20d9a0fbc513b9171cc85006e432bb8
igorsimb <igor.simbirtsev@gmail.com>
2024-11-26 13:38:32 +0300

    feature(portolio): add MP Monitor
    Add MP Monitor as a project in the Portfolio section.

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import IndexView, Ez2TaskView, StoreProjectView
+from .views import IndexView, Ez2TaskView, StoreProjectView, MPMonitorView
 
 app_name = "core"
 urlpatterns = [
-    path('', IndexView.as_view(), name='main'),
-    path('ez2task-project/', Ez2TaskView.as_view(), name='ez2task'),
-    path('store-project/', StoreProjectView.as_view(), name='store_project'),
+    path("", IndexView.as_view(), name="main"),
+    path("mp-monitor-project/", MPMonitorView.as_view(), name="mp_monitor"),
+    path("ez2task-project/", Ez2TaskView.as_view(), name="ez2task"),
+    path("store-project/", StoreProjectView.as_view(), name="store_project"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,9 @@
-from django.utils.translation import gettext as _
-from django.core.mail import send_mail
-from django.views.generic import TemplateView, DetailView
 from django.contrib.auth import get_user_model
-from django.shortcuts import redirect, render
+from django.core.mail import send_mail
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.utils.translation import gettext as _
+from django.views.generic import TemplateView
 
 from core.forms import ContactForm
 
@@ -11,12 +11,12 @@ User = get_user_model()
 
 
 class IndexView(TemplateView):
-    template_name = 'core/index.html'
+    template_name = "core/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         form = ContactForm(self.request.POST or None)
-        context['form'] = form
+        context["form"] = form
 
         return context
 
@@ -25,27 +25,33 @@ class IndexView(TemplateView):
         context = self.get_context_data()
         data = request.POST
         if form.is_valid():
-            name = data.get('name')
-            email = data.get('email')
-            subject = data.get('subject')
-            message = data.get('message')
+            name = data.get("name")
+            email = data.get("email")
+            subject = data.get("subject")
+            message = data.get("message")
 
-            str_msg = f'{message}\nSent by: {name} ({email})'
+            str_msg = f"{message}\nSent by: {name} ({email})"
 
             send_mail(
-                subject, str_msg,
-                email, ['igor.simbirtsev@gmail.com'],
+                subject,
+                str_msg,
+                email,
+                ["igor.simbirtsev@gmail.com"],
             )
-            return redirect('main')
+            return redirect("main")
         else:
-            HttpResponse(_('Something went wrong. Please try again.'))
+            HttpResponse(_("Something went wrong. Please try again."))
 
-        return render(request, 'core/index.html', context)
+        return render(request, "core/index.html", context)
+
+
+class MPMonitorView(TemplateView):
+    template_name = "core/projects/mp_monitor_project.html"
 
 
 class Ez2TaskView(TemplateView):
-    template_name = 'core/projects/ez2task_project.html'
+    template_name = "core/projects/ez2task_project.html"
 
 
 class StoreProjectView(TemplateView):
-    template_name = 'core/projects/store_project.html'
+    template_name = "core/projects/store_project.html"
